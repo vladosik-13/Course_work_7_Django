3aac2a922b7e278ce3b5e484c91e327a6e6d787d
Владислав Кошелев <testvlad59@gmail.com>
2025-01-26 22:14:28 +0300

    реализованы модели Рассылка (class Mailing) и Попытка рассылки (class MailingAttempt). созданы и  применены миграции

--- newsletter/models.py
+++ newsletter/models.py
@@ -24,3 +24,39 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
+
+
+
+class Mailing(models.Model):
+    STATUS_CHOICES = [
+        ('CREATED', 'Создана'),
+        ('STARTED', 'Запущена'),
+        ('COMPLETED', 'Завершена'),
+    ]
+
+    start_time = models.DateTimeField(verbose_name='Дата и время первой отправки')
+    end_time = models.DateTimeField(verbose_name='Дата и время окончания отправки')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='CREATED', verbose_name='Статус')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='mailings', verbose_name='Сообщение')
+    clients = models.ManyToManyField(Client, related_name='mailings', verbose_name='Получатели')
+
+    def __str__(self):
+        return f"Рассылка {self.id} - {self.status}"
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
+class MailingAttempt(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, related_name='attempts', verbose_name='Рассылка')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время попытки')
+    success = models.BooleanField(default=False, verbose_name='Успешно')
+    response = models.TextField(blank=True, verbose_name='Ответ почтового сервера')
+
+    def __str__(self):
+        return f"Попытка {self.id} - {'Успешно' if self.success else 'Не успешно'}"
+
+    class Meta:
+        verbose_name = 'Попытка рассылки'
+        verbose_name_plural = 'Попытки рассылок'
