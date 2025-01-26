707f2c731d28ebe9e78c457759c1ac4ff9b7123d
Владислав Кошелев <testvlad59@gmail.com>
2025-01-26 22:04:15 +0300

    для админки зарегистрированы модели Client и Message

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Client, Message
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'comment',)
+    search_fields = ('name', 'email',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'text_message',)
+    search_fields = ('subject', 'text_message',)
+    list_filter = ('subject', 'text_message',)
