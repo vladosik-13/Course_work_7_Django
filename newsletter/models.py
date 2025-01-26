1e37b4022daf539ace7ff065d2414444b8bdd806
Владислав Кошелев <testvlad59@gmail.com>
2025-01-26 21:45:35 +0300

    реализована модель управления клиентами (class Client) и модель управления сообщениями (class Message) произведены миграции, создан superuser, создана и настроена БД

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    name = models.CharField(max_length=200, verbose_name="ФИО")
+    comment = models.TextField(max_length=500, verbose_name="Комментарий")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255, verbose_name='Тема письма')
+    text_message = models.TextField(verbose_name='Тело письма')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
