76d27880f7ae945273fa5a32f0230cb4c94c42a3
Владислав Кошелев <testvlad59@gmail.com>
2025-01-27 22:35:11 +0300

    ПРименена стилизация форм

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,12 +6,67 @@ class ClientForm(forms.ModelForm):
         model = Client
         fields = ['email', 'name', 'comment']
 
+    def __init__(self, *args, **kwargs):
+        super(ClientForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите Email'
+        })
+
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите Ф.И.О.'
+        })
+
+        self.fields['comment'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите комментарий'
+        })
+
+
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['subject', 'text_message']
 
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+
+        self.fields['subject'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Тема сообщения'
+        })
+
+        self.fields['text_message'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Текст сообщения'
+        })
+
+
 class MailingForm(forms.ModelForm):
     class Meta:
         model = Mailing
         fields = ['start_time', 'end_time', 'status', 'message', 'clients']
+
+    def __init__(self, *args, **kwargs):
+        super(MailingForm, self).__init__(*args, **kwargs)
+
+        self.fields['start_time'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Дата начала рассылки'
+        })
+
+        self.fields['end_time'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Дата окончания рассылки'
+        })
+
+        self.fields['status'].widget.attrs.update({
+            'class': 'form-control'})
+
+        self.fields['message'].widget.attrs.update({
+            'class': 'form-control'})
+
+        self.fields['clients'].widget.attrs.update({
+            'class': 'form-control'})
