8490fe4cbcfcbe7abf54b9c8334fe3deda231c48
Владислав Кошелев <testvlad59@gmail.com>
2025-01-27 23:19:41 +0300

    реализован модуль views.py для приложения users полностью в CBV-представлении

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView, UpdateView, DetailView
+)
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Create your views here.
+# Регистрация пользователя
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/signup.html'
+
+# Вход пользователя
+class LoginView(auth_views.LoginView):
+    template_name = 'users/login.html'
+    success_url = reverse_lazy('home')
+
+# Выход пользователя
+class LogoutView(auth_views.LogoutView):
+    next_page = reverse_lazy('home')
+
+# Профиль пользователя
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = CustomUser
+    template_name = 'users/profile.html'
+    context_object_name = 'user_profile'
+
+    def get_object(self):
+        return self.request.user
+
+# Редактирование профиля пользователя
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    template_name = 'users/profile_edit.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        return self.request.user
